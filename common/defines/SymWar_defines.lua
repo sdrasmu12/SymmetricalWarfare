10936726eacd4a30a2459fc3b72f47c6c92990a2
sdrasmu12 <sdrasmu12@earlham.edu>
2016-05-24 23:30:21 -0400

    removed accuracy bonus for station sensors

--- common/defines/SymWar_defines.lua
+++ common/defines/SymWar_defines.lua
@@ -1,1 +1,13 @@
+NDefines.NCamera.ENTER_SYSTEM_ZOOM_STEP = 8 -- 6  Controls which zoom step is used after entering system
+NDefines.NCamera.SYSTEM_FOCUS_PLANET_STEP = 5 -- 2  Controls which zoom step to planet zoom to in a system
+NDefines.NCamera.ZOOM_STEPS_SYSTEM_PERCENTAGES = { 0.025, 0.05, 0.075, 0.1, 0.15, 0.25, 0.5, 1.0, 1.5, 3.0 } -- (0.025, 0.1, 0.25, 0.5, 1.0, 1.5, 3.0) System zoom steps, in percent relative to the system size		
+
+NDefines.NCombat.COMBAT_BACK_OFF_RATE = 0.30
+NDefines.NCombat.COMBAT_DETECT_RANGE_MULT = 1	-- Range to engage in combat with other ships is <fleets max attack range> * COMBAT_DETECT_RANGE_MULT
+NDefines.NCombat.FLEET_MOVE_MAX_DIST_SHIPS = 60.0	-- Max distance that ships are allowed to travel before last ship
+
 NDefines.NGraphics.SHIP_RANDOM_HEIGHT_OFFSET = 60.0
+NDefines.NGraphics.BALLISTIC_PROJECTILE_MISSED_LIFETIME = 3.0 -- missed ballistic projectiles will live for (at least) this long before being removed
+NDefines.NGraphics.MISSED_BEAM_LENGTH = 750.0 -- how long missed beams will be
+
+NDefines.NShip.FLEET_BASE_FORMATION_SCALE = 4 
